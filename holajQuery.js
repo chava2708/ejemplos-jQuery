feefb41ae853e892fee6955d734c3f0d8c98d03e
chava2708 <salvador95_2@hotmail.com>
2017-03-14 12:20:10 -0600

    a

---
+++ holajQuery.js
@@ -0,0 +1,18 @@
+/*$(document).ready(saluda)
+
+// function saluda(){
+// alert($('h1').text())
+// }*/
+
+// $(function(){
+//     //alert($('h1').text())
+
+//     //Se crea un parrafo nuevo en un objeto jQuery
+//     $nuevo = $('<p>Parrafo Nuevo </p>')
+//     $('h2').after($nuevo);
+    
+//     $('#boton1').click(function (e) {         
+        
+
+//     });
+// })
